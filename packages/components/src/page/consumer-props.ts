94e47ae38a4303cef291e6b61fb7ca1255a2bad0
hubert <hubert@lejian.com>
2022-10-18 20:41:09 +0800

    feat: page 组件 css vars 添加

--- packages/components/src/page/consumer-props.ts
+++ packages/components/src/page/consumer-props.ts
@@ -1,3 +1,11 @@
-export const PageConsumerProps: { containerWidth: string } = {
+// Types
+import type { PageProps } from './index'
+
+export const PageConsumerProps: Required<
+  Pick<PageProps, 'containerWidth' | 'themeVars'>
+> = {
   containerWidth: '1120px',
-};
+  themeVars: {
+    primary: '#046bde˝'
+  },
+}
