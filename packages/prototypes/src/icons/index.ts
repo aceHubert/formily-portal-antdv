b65ee0ee5ca276baa1c6c0e76f69791480860bd8
hubert <hubert@lejian.com>
2023-02-23 16:10:46 +0800

    fix: @ckeditor 编译问题
    * umd 作为外部引用
    * 修改 sources 图标

---
+++ packages/prototypes/src/icons/index.ts
@@ -0,0 +1,4 @@
+import { GlobalRegistry } from '@designable/core'
+import * as sources from './sources'
+
+GlobalRegistry.registerDesignerIcons(sources)
--- packages/prototypes/src/index.ts
+++ packages/prototypes/src/index.ts
@@ -1,3 +1,4 @@
+import './icons'
 export * from './components'
 export * from './locales'
 export * from './schemas'
